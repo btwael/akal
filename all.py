4692921dceac0ee69871d287712523790d339c8a
Wael Boutglay <btwael@gmail.com>
2018-12-01 23:40:01 +0100

    build and running scripts

--- all.py
+++ all.py
@@ -3,7 +3,7 @@ from poyo import parse_string, PoyoException
 
 
 def compileCpp(input, output, define_list):
-    l = "aarch64-elf-g++ -Isystem -Isystem/include -march=armv8-a -mtune=cortex-a53 -mlittle-endian -mcmodel=small -Wall -fno-builtin -nostdinc -nostdlib -fno-exceptions -fno-rtti -std=c++11 -c "
+    l = "clang -target aarch64-none-elf -Isystem -Isystem/include -Wall -fno-builtin -nostdinc -nostdlib -fno-exceptions -fno-rtti -std=c++11 -c "
     l = l + input + " -o " + output
     for define in define_list:
         l += " -D" + define
@@ -29,7 +29,8 @@ if config["target"].startswith("rpi3"):
 
 compileCpp("./application/main.cc", "./build/main.o", pp_define)
 system(
-    "aarch64-elf-g++ -Wall -ffreestanding -nostdinc -nostdlib -c ./system/src/board/rpi3/startup.S -o ./build/startup.o")
+    "clang -target aarch64-none-elf -Wall -ffreestanding -nostdinc -nostdlib -c ./system/src/board/rpi3/startup.S -o ./build/startup.o")
 system(
-    "aarch64-elf-ld -nostdlib -nostartfiles ./build/startup.o ./build/main.o -T ./system/src/board/rpi3/link.ld -o ./build/kernel8.elf")
-system("aarch64-elf-objcopy -O binary ./build/kernel8.elf ./build/kernel8.img")
+    "aarch64-none-elf-ld -nostdlib -nostartfiles ./build/startup.o ./build/main.o -T ./system/src/board/rpi3/link.ld -o ./build/kernel8.elf")
+system("aarch64-none-elf-objcopy -O binary ./build/kernel8.elf ./build/kernel8.img")
+system("qemu-system-aarch64 -M raspi3 -kernel ./build/kernel8.img -serial null -serial stdio")
---
+++ run.py
@@ -0,0 +1,3 @@
+from os import system
+
+system("qemu-system-aarch64 -M raspi3 -kernel ./build/kernel8.img -serial null -serial stdio")
