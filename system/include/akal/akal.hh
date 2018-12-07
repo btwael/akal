d61e574004887b326eff349a910c94806ab79403
Wael Boutglay <btwael@gmail.com>
2018-12-07 21:00:43 +0100

    Required for pure virtual method

--- system/include/akal/akal.hh
+++ system/include/akal/akal.hh
@@ -7,6 +7,6 @@ extern "C" int __cxa_atexit( void *object, void (*destructor)(void *), void *dso
 
 void* __dso_handle = nullptr;
 
-/*extern "C" void __cxa_pure_virtual() {}*/
+extern "C" void __cxa_pure_virtual() {}
 
 #include "src/include.cc"
